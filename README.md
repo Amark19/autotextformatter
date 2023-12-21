10b7dfcfbef1f6e3012617305e7e794f9f5bf0f3
Amark19 <amarkhamkar6@gmail.com>
2023-12-22 00:38:37 +0530

    autotextformatter first commit

---
+++ README.md
@@ -0,0 +1,91 @@
+
+
+# AutoTextformatter
+
+A Flutter package that automatically adds hyphen or increments numbers to the next line if the current line has one.
+
+## Features
+
+Add this to your flutter app to:
+- Enhance the text editor experience, similar to Google Keep or Google Docs.
+- Automatically enable your text controller to add hyphens or numbering at the start of the line, improving the overall appearance of the text editor.
+
+## Getting Started
+
+To use the `autotextformatter` Flutter package, include it in your `pubspec.yaml` file:
+
+```yaml
+dependencies:
+  autotextformatter: ^1.0.0  # Use the actual version you want
+```
+
+Run the following command to install the package:
+
+```dart
+flutter pub get
+```
+
+## Usage
+Import the `autotextformatter` package in your Dart file:
+
+```dart
+import 'package:autotextformatter/autotextformatter.dart';
+```
+Use the package to automatically format text:
+
+```dart
+import 'package:flutter/material.dart';
+import "package:autolineadjuster/autolineadjuster.dart";
+
+void main() {runApp(const MyApp());}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Autohyphenation'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController descController = TextEditingController();
+  late LineAdjuster lineAdjuster =
+      LineAdjuster(targetController: descController);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: TextFormField(
+        controller: descController,
+        maxLines: null,
+        textInputAction: TextInputAction.newline,
+        onChanged: (value) {
+          lineAdjuster.Adjust();
+        },
+      ),
+    );
+  }
+}
+```
+
+## Additional information
+- Maintainer ([Amar khamkar](https://github.com/Amark19)) 
+- License ([MIT]())
---
+++ lib/autotextformatter.dart
@@ -0,0 +1,92 @@
+library autolineadjuster;
+
+import 'package:flutter/cupertino.dart';
+
+/// A Calculator.
+class LineAdjuster {
+  String currentText = "", previousText = "";
+  late TextEditingController targetController ;
+  LineAdjuster({required TextEditingController targetController}) {
+    this.targetController = targetController;
+  }
+
+   void Adjust() {
+    final value = targetController.value;
+    final cursorPosition = targetController.selection.base.offset;
+    final text = value.text;
+    final selection = value.selection;
+    final textList = text.substring(0, selection.extentOffset).split('\n');
+    currentText =
+        textList.length <= 1 ? textList[0] : textList[textList.length - 2];
+    final trimmedTxt = currentText.trim();
+    if (text.substring(0, selection.extentOffset).endsWith("\n")) {
+      if (currentText.length > 0 &&
+          (_checkForNumbersMatch(trimmedTxt) || trimmedTxt[0] == "-") &&
+          text.length > previousText.length) {
+        if (trimmedTxt[0] == "-") {
+          final newCursorPosition =
+              cursorPosition + _getStartSpacesInLine(currentText) + 1;
+          _updateTargetController(
+              targetController, text, selection, newCursorPosition, "-");
+        } else if (_checkForNumbersMatch(trimmedTxt)) {
+          String strNumbers = _getNumbersInStart(trimmedTxt);
+          int numbers = strNumbers.length;
+          final newCursorPosition = cursorPosition +
+              (RegExp(r"^9+$").hasMatch(strNumbers.toString())
+                  ? numbers + 1
+                  : numbers) +
+              _getStartSpacesInLine(currentText) +
+              1;
+          _updateTargetController(
+              targetController,
+              text,
+              selection,
+              newCursorPosition,
+              "${int.parse(trimmedTxt.substring(0, numbers)) + 1}${trimmedTxt[numbers]}");
+        }
+      }
+    }
+    previousText = text;
+  }
+
+  int _getStartSpacesInLine(String txt) {
+    int spaces = 0;
+    for (int i = 0; i < txt.length; i++) {
+      if (txt[i] == " ") {
+        spaces++;
+      } else {
+        break;
+      }
+    }
+    return spaces;
+  }
+
+  String _getNumbersInStart(String txt) {
+    int numbers = 0;
+    String strNumbers = "";
+    for (int i = 0; i < txt.length; i++) {
+      if (RegExp("[0-9]").hasMatch(txt[i])) {
+        strNumbers += txt[i];
+      } else {
+        break;
+      }
+    }
+    return strNumbers;
+  }
+
+  bool _checkForNumbersMatch(String txt) {
+    return RegExp("^[0-9]+[)|.]").hasMatch(txt);
+  }
+
+  TextEditingValue _updateTargetController(
+      controller, text, selection, newCursorPosition, value) {
+    controller.value = TextEditingValue(
+      text: text.substring(0, selection.extentOffset) +
+          " " * _getStartSpacesInLine(currentText) +
+          "$value" +
+          text.substring(selection.extentOffset, text.length),
+      selection: TextSelection.collapsed(offset: newCursorPosition),
+    );
+    return controller.value;
+  }
+}
